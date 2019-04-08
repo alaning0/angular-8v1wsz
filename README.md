9b47cf9d028983adc4e7f14f6e1aace732ec1bb7
alaning0 <thepoisonapple2004@gmail.com>
2019-04-08 22:32:18 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8v1wsz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8v1wsz)
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero = 'Windstorm';
+
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  //heroes = HEROES;
+  heroes: Hero[];
+
+  //constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
